e788ca032d23511730b17a4d8fe31ef5d90f6fb8
Roohollah81 <m.roohollah.v@gmail.com>
2024-11-16 11:29:10 +0330

    add "add user" function

--- main.js
+++ main.js
@@ -3,10 +3,17 @@ const type = "A";
 function create_user_panel() {
   const panel_format = ["name", "age", "city"];
   console.log(panel_format);
+  return panel_format;
+}
+
+function add_user(name, age, city) {
+  const panel_format = create_user_panel();
+  let new_user = {};
 }
 
 function main() {
   create_panel();
+  add_user("ali", "20", "tabriz");
 }
 
 main();
