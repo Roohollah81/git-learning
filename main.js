c9738939bd15e026b4d4b12bd34fe7018ab600b0
Roohollah81 <m.roohollah.v@gmail.com>
2024-11-16 11:25:09 +0330

    change function name

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const type = "A";
 
-function create_panel() {
+function create_user_panel() {
   const panel_format = ["name", "age", "city"];
   console.log(panel_format);
 }
+
 function main() {
   create_panel();
 }
